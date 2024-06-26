ad18c0c556583b8de0afd9c5de18482bacf61da5
Marxu <fuhaojun@52tt.com>
2024-06-26 16:44:03 +0800

    feat: 先画出页面 使用 plotly_chart  画出图表📈， get_data 先给假数据📊

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import plotly.express as px
 
 st.title('Weather Forecast for the Next Days')
 place = st.text_input('Place: ')
@@ -7,3 +8,14 @@ days = st.slider('Forecast Days', min_value=1, max_value=5,
 option = st.selectbox('Select data to view', ('Temperature', 'Sky'))
 
 st.subheader(f'{option} for the next {days} days in {place}')
+
+def get_data(days):
+    date = ['2024-25-6', '2024-26-6', '2024-27-6']
+    temperature = [10, 11, 15]
+    temperature = [days * i for i in temperature]
+    return date,temperature
+
+d, t = get_data(days)
+
+figure = px.line(x = d,y = t,labels={'x': 'Date', 'y': 'Temperature'})
+st.plotly_chart(figure)
