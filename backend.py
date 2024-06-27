007e13b549854761a57d0dcb9f0ac49d2801a21c
Marxu <fuhaojun@52tt.com>
2024-06-27 15:48:09 +0800

    feat: 准备 后端数据， 拿到整个数据后 切割 取到需要的部分

--- backend.py
+++ backend.py
@@ -6,7 +6,19 @@ def get_data(place, forecast_days=None, kind=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
-    return data
+    # 拿 list 里的数据
+    filter_data = data['list']
+    # 每天统计 8个点
+    nr_days = 8 * forecast_days
+    filter_data = filter_data[:nr_days]
+    if kind == 'Temperature':
+        # filter_data 中的每一个字典的 main 键下的 temp 值被提取出来，组成一个新的列表
+        # 比如 filter_data = [20.5, 22.3, 19.8]
+        filter_data = [dict['main']['temp'] for dict in filter_data]
+    if kind == 'Sky':
+        filter_data = [dict['weather'][0]['temp'] for dict in filter_data]
+
+    return filter_data
 
 if __name__ == '__main__':
-    print(get_data(place='Guangzhou'))
+    print(get_data(place='Guangzhou', forecast_days=3, kind='Temperature'))
