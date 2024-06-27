587dd47d5dc4ec64fc2b2ff92828bee88af57a49
Marxu <fuhaojun@52tt.com>
2024-06-27 17:11:44 +0800

    feat: 用接口数据 展示 温度线和 展示天气☁️图标

--- backend.py
+++ backend.py
@@ -2,7 +2,7 @@ import requests
 
 API_KEY = '8ba57a6732a06cf6ca72eb87a7a06917'
 
-def get_data(place, forecast_days=None, kind=None):
+def get_data(place, forecast_days=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
@@ -11,14 +11,14 @@ def get_data(place, forecast_days=None, kind=None):
     # 每天统计 8个点
     nr_days = 8 * forecast_days
     filter_data = filter_data[:nr_days]
-    if kind == 'Temperature':
-        # filter_data 中的每一个字典的 main 键下的 temp 值被提取出来，组成一个新的列表
-        # 比如 filter_data = [20.5, 22.3, 19.8]
-        filter_data = [dict['main']['temp'] for dict in filter_data]
-    if kind == 'Sky':
-        filter_data = [dict['weather'][0]['temp'] for dict in filter_data]
+    # if kind == 'Temperature':
+    #     # filter_data 中的每一个字典的 main 键下的 temp 值被提取出来，组成一个新的列表
+    #     # 比如 filter_data = [20.5, 22.3, 19.8]
+    #     filter_data = [dict['main']['temp'] for dict in filter_data]
+    # if kind == 'Sky':
+    #     filter_data = [dict['weather'][0]['temp'] for dict in filter_data]
 
     return filter_data
 
 if __name__ == '__main__':
-    print(get_data(place='Guangzhou', forecast_days=3, kind='Temperature'))
+    print(get_data(place='Guangzhou', forecast_days=3))
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import plotly.express as px
 
+from backend import get_data
+
 st.title('Weather Forecast for the Next Days')
 place = st.text_input('Place: ')
 days = st.slider('Forecast Days', min_value=1, max_value=5,
@@ -9,13 +11,29 @@ option = st.selectbox('Select data to view', ('Temperature', 'Sky'))
 
 st.subheader(f'{option} for the next {days} days in {place}')
 
-def get_data(days):
-    date = ['2024-25-6', '2024-26-6', '2024-27-6']
-    temperature = [10, 11, 15]
-    temperature = [days * i for i in temperature]
-    return date,temperature
+# def get_data(days):
+#     date = ['2024-25-6', '2024-26-6', '2024-27-6']
+#     temperature = [10, 11, 15]
+#     temperature = [days * i for i in temperature]
+#     return date,temperature
 
-d, t = get_data(days)
+if place:
+    filter_data = get_data(place, days)
 
-figure = px.line(x = d,y = t,labels={'x': 'Date', 'y': 'Temperature'})
-st.plotly_chart(figure)
+    if option == 'Temperature':
+        temperature = [dict['main']['temp'] for dict in filter_data]
+        dates = [dict['dt_txt'] for dict in filter_data]
+        figure = px.line(x = dates,y = temperature,labels={'x': 'Dates', 'y': 'Temperature'})
+        st.plotly_chart(figure)
+
+    if option == 'Sky':
+        images = {
+            'Clear': 'images/clear.png',
+            'Clouds': 'images/cloud.png',
+            'Rain': 'images/rain.png',
+            'Snow': 'images/snow.png'
+        }
+        sky_condition = [dict['weather'][0]['main'] for dict in filter_data]
+        image_path = [images[condition] for condition in sky_condition]
+        print(image_path)
+        st.image(image_path, width=115)
